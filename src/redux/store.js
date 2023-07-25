cdad59c08969e2accd9333180fb703562a496018
Ignacio <81566680+nacholaciar@users.noreply.github.com>
2023-07-25 20:06:54 -0300

    @reduxjs/toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 
-const store = createStore(reducer);
-
-export default store;
+export default configureStore({
+    reducer: reducer,
+});
